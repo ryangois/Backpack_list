1715c2a17a69c31c821641e9f5e073d991ca751c
ryangois <ryangoisdev@gmail.com>
2023-04-02 19:14:23 -0300

    creating validations

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const form = document.getElementById("newItem");
-
+const list = document.getElementById("list");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
-    console.log(evento.target.elements["name"].value)
-    console.log(evento.target.elements["quantity"].value)
 
-    console.log("worked")
-    console.log(event)
-
+    newElement(event.target.elements["name"].value, event.target.elements["quantity"].value)
 })
+
+function newElement(name, quantity) {
+    console.log(name);
+    console.log(quantity);
+
+    const newItem = document.createElement("li");
+    newItem.classList.add("item");
+
+    const itemNumber = document.createElement("strong")
+    itemNumber.innerHTML = quantity
+
+    newItem.appendChild(itemNumber);
+    newItem.innerHTML += name;
+
+    list.appendChild(newItem);
+}
