31f04fd55c843d113ff333990217411058876e64
ryangois <ryangoisdev@gmail.com>
2023-04-02 19:02:16 -0300

    data capture

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const form = document.getElementById("newItem");
 
 
-form.addEventListener("submit", (evento) => {
-    evento.preventDefault()
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
     console.log(evento.target.elements["name"].value)
     console.log(evento.target.elements["quantity"].value)
 
     console.log("worked")
-    console.log(evento)
+    console.log(event)
+
 })
